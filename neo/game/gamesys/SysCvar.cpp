b2c2d0f803549b3103197848d30ecc3d4ef05fb7
maxutka_3006 <191056081+maxutka3006@users.noreply.github.com>
2026-09-23 08:25:13 +0200

    Mark 'pm_ignoreVacuum' as CVAR_CHEAT

--- neo/game/gamesys/SysCvar.cpp
+++ neo/game/gamesys/SysCvar.cpp
@@ -270,7 +270,7 @@ idCVar pm_thirdPerson(				"pm_thirdPerson",			"0",			CVAR_GAME | CVAR_NETWORKSYN
 idCVar pm_thirdPersonDeath(			"pm_thirdPersonDeath",		"0",			CVAR_GAME | CVAR_NETWORKSYNC | CVAR_BOOL, "enables third person view when player dies" );
 idCVar pm_modelView(				"pm_modelView",				"0",			CVAR_GAME | CVAR_NETWORKSYNC | CVAR_INTEGER, "draws camera from POV of player model (1 = always, 2 = when dead)", 0, 2, idCmdSystem::ArgCompletion_Integer<0,2> );
 idCVar pm_airTics(					"pm_air",					"1800",			CVAR_GAME | CVAR_NETWORKSYNC | CVAR_INTEGER, "how long in milliseconds the player can go without air before he starts taking damage" );
-idCVar pm_ignoreVacuum(				"pm_ignoreVacuum",			"0",			CVAR_GAME | CVAR_BOOL, "ignores air/vacuum checks, allowing normal movement in open areas with info_vacuum (set newAirless to false)" );
+idCVar pm_ignoreVacuum(				"pm_ignoreVacuum",			"0",			CVAR_GAME | CVAR_BOOL | CVAR_CHEAT, "ignores air/vacuum checks, allowing normal movement in open areas with info_vacuum (set newAirless to false)" );
 
 idCVar g_showPlayerShadow(			"g_showPlayerShadow",		"0",			CVAR_GAME | CVAR_ARCHIVE | CVAR_BOOL, "enables shadow of player model" );
 idCVar g_showHud(					"g_showHud",				"1",			CVAR_GAME | CVAR_ARCHIVE | CVAR_BOOL, "" );
